faba50f6ed42845d01afa6594cbe5ccd55ff4933
Naveen Kumar <naveenkumarnb76@gmail.com>
2023-12-28 21:40:01 +0530

    First commit

---
+++ app/Contact/layout.js
@@ -0,0 +1,11 @@
+
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body>
+//         Welcome to contact page
+//         {children}</body>
+//     </html>
+//   )
+// }
